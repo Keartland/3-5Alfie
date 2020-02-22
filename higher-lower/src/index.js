d3d179c4567d8021d126d3629c9e9c78a9fdcc92
Harry <harrykeartland@gmail.com>
2020-02-22 02:52:36 +0000

    done the bulk

--- higher-lower/src/index.js
+++ higher-lower/src/index.js
@@ -2,34 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 class Item extends React.Component {
   render() {
     return (
       <div className="Item">
-        <img src={this.props.thumbnail}></img>
+        <img src={this.props.thumbnail} alt=""></img>
         <p>{this.props.name}</p>
       </div>
 
-    );
-  }
-}
-
-class Choice extends React.Component {
-  render() {
-    const optionsList = !this.props.left ? this.props.value.options.map((option) =>
-            <div>
-              <button>{option} times</button>
-            </div>
-    ):
-    <div>
-    {this.props.value.carbon}
-    </div>;
-    return (
-      <div className="Choice">
-        <Item name={this.props.value.name} thumbnail={this.props.value.thumbnail} />
-        <ul>{optionsList}</ul>
-      </div>
     );
   }
 }
@@ -39,14 +19,12 @@ class ListItem extends React.Component {
         const name = this.props.name;
         const thumbnail = this.props.thumbnail;
         const amount = this.props.amount;
-        const unit = this.props.unit;
-
         return (
             <div className="ListItem">
-                <img src={thumbnail}></img>
+                <img src={thumbnail} alt=""></img>
                 <div className="Description">
                     <p>{name}</p>
-                    <p>{DATASOURCE[current].carbon/amount}</p>
+                    <p>{DATASOURCE[this.props.current].carbon/amount}</p>
                 </div>
             </div>
         );
@@ -55,30 +33,69 @@ class ListItem extends React.Component {
 
 class List extends React.Component {
     render() {
-        const items = this.props.items;
+      const current = this.props.current
+        const items = this.props.items.filter(function (el) {
+          return (DATASOURCE[current].carbon/el.carbon) > 1
+        });
 
         const listItems = items.map((item) =>
-            <div>
+              <div>
                 <li key={item.name}>
-                    <ListItem name={item.name} thumbnail={item.thumbnail} amount={item.carbon}/>
+                  <ListItem current={this.props.current} name={item.name} thumbnail={item.thumbnail} amount={item.carbon}/>
                 </li>
-            </div>
-        );
+              </div>
+          );
         return (
             <ul>{listItems}</ul>
         );
     }
+  }
+
+class Choice extends React.Component {
+  handleClick(option, currentChoice){
+    if (option === currentChoice.correct){
+      this.props.correctButtonClicked()
+    }
+  }
+  render() {
+    const currentChoice = !this.props.left ? DATASOURCE[this.props.current + 1] : DATASOURCE[this.props.current]
+    const optionsList = !this.props.left ? currentChoice.options.map((option) =>
+            <div key={option}>
+              <button onClick={() => this.handleClick(option,currentChoice)}>{option} times</button>
+            </div>
+    ):
+    <div>
+    {currentChoice.carbon}
+    </div>;
+    return (
+      <div className="Choice">
+        <Item name={currentChoice.name} thumbnail={currentChoice.thumbnail} />
+        <ul>{optionsList}</ul>
+        <a href={currentChoice.source}>source</a>
+      </div>
+    );
+  }
 }
 
+
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {score:0, current:0};
+  }
+
+  onCorrectButtonClicked = () => {
+    this.setState({score:this.state.score+1,current:this.state.current+1})
+  }
+
   render() {
     return (
       <div className="game">
-        <List items={DATASOURCE}/>
+        <List items={DATASOURCE} current={this.state.current}/>
         <div className="choices">
-          <Choice left={true} value={DATASOURCE[current]}/>
-          <p>vs</p>
-          <Choice left={false} value={DATASOURCE[current+1]}/>
+          <Choice left={true} current={this.state.current}/>
+          <Choice left={false} correctButtonClicked={this.onCorrectButtonClicked} current={this.state.current}/>
+          <p>{this.state.score}</p>
         </div>
       </div>
     );
@@ -86,12 +103,12 @@ class Game extends React.Component {
 }
 
 
-const current = 1
+
 
 const DATASOURCE = [
-  {name:"1kg Cheese", thumbnail:"placeholder.png", carbon:13.5, source:"link.com"},
-  {name:"1kg Beef", thumbnail:"placeholder.png", carbon:27, source:"link.com", options:[4,2,10]},
-  {name:"1kg Lamb", thumbnail:"placeholder.png", carbon:39.2, source:"link.com", options:[4,1.5,10]}
+  {name:"1kg Cheese", thumbnail:"placeholder.png", carbon:13.5, source:"http://link.com"},
+  {name:"1kg Beef", thumbnail:"placeholder.png", carbon:27, source:"http://link.com", options:[4,2,10], correct:2},
+  {name:"1kg Lamb", thumbnail:"placeholder.png", carbon:39.2, source:"http://link.com", options:[4,1.5,10],correct:1.5}
 ]
 // ========================================
 
